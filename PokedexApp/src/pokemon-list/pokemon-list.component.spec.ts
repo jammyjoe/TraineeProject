406fe06a152c5fd52bf16914654eb43cfb6230d3
Jamil Munayem <jamilmunayem@hotmail.com>
2024-06-14 14:20:31 +0100

    Move header to shared index

--- PokedexApp/src/pokemon-list/pokemon-list.component.spec.ts
+++ PokedexApp/src/pokemon-list/pokemon-list.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PokemonListComponent } from './pokemon-list.component';
+// import { PokemonListComponent } from './pokemon-list.component';
 
-describe('PokemonListComponent', () => {
-  let component: PokemonListComponent;
-  let fixture: ComponentFixture<PokemonListComponent>;
+// describe('PokemonListComponent', () => {
+//   let component: PokemonListComponent;
+//   let fixture: ComponentFixture<PokemonListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PokemonListComponent]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [PokemonListComponent]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(PokemonListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(PokemonListComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- PokedexApp/src/pokemon-list/pokemon-list.component.ts
+++ PokedexApp/src/pokemon-list/pokemon-list.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Pokemon } from '../models/pokemon.model';
-import { PokemonService } from '../services/pokemon.service';
+// import { Component, OnInit } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { Pokemon } from '../models/pokemon.model';
+// import { PokemonService } from '../services/pokemon.service';
 
-@Component({
-  selector: 'app-pokemon-list',
-  templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
-})
-export class PokemonListComponent implements OnInit {
-  pokemons$: Observable<Pokemon[]> = new Observable<Pokemon[]>(); // Initialize here
+// @Component({
+//   selector: 'app-pokemon-list',
+//   templateUrl: './pokemon-list.component.html',
+//   styleUrls: ['./pokemon-list.component.css']
+// })
+// export class PokemonListComponent implements OnInit {
+//   pokemons$: Observable<Pokemon[]> = new Observable<Pokemon[]>(); // Initialize here
 
-  constructor(private pokemonService: PokemonService) { }
+//   constructor(private pokemonService: PokemonService) { }
 
-  ngOnInit(): void {
-    this.getPokemons();
-  }
+//   ngOnInit(): void {
+//     this.getPokemons();
+//   }
 
-  getPokemons(): void {
-    this.pokemons$ = this.pokemonService.getPokemons();
-  }
-}
+//   getPokemons(): void {
+//     this.pokemons$ = this.pokemonService.getPokemons();
+//   }
+// }
