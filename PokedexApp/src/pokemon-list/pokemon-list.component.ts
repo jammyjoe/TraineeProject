e1ae94ffaaaae47db554a93eb49c25f6502f915b
Jamil Munayem <jamilmunayem@hotmail.com>
2024-06-28 15:37:20 +0100

    Fix warnings

--- PokedexApp/src/pokemon-list/pokemon-list.component.ts
+++ PokedexApp/src/pokemon-list/pokemon-list.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input } from '@angular/core';
-import { Pokemon } from '../app/shared/models/pokemon.model';
+// import { Component, Input } from '@angular/core';
+// import { Pokemon } from '../app/shared/models/pokemon.model';
+// import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-pokemon-list',
-  standalone: true,
-  templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
-})
-export class PokemonListComponent {
-  @Input() pokemons: Pokemon[] = [];
+// @Component({
+//   selector: 'app-pokemon-list',
+//   standalone: true,
+//   templateUrl: './pokemon-list.component.html',
+//   styleUrls: ['./pokemon-list.component.css'],
+//   imports: [CommonModule]
+// })
+// export class PokemonListComponent {
+//   @Input() pokemons: Pokemon[] = [];
 
-  constructor() { }
-}
+//   constructor() { }
+// }
