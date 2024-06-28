7896c63b970d94a36c1e4a9d60c42af56656c42d
Jamil Munayem <jamilmunayem@hotmail.com>
2024-06-28 15:07:56 +0100

    Add edit

--- PokedexApp/src/app/home/app.routes.ts
+++ PokedexApp/src/app/home/app.routes.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes, provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SearchComponent } from '../search/search.component';
 import { AddComponent } from '../add/add.component';
+import { EditComponent } from '../edit/edit.component';
 
 export const routes: Routes = [
     // { path: '', component: AppComponent }, Temp fix until I add home component as route duplicates
     { path: 'search', component: SearchComponent },
-    { path: 'add', component: AddComponent }
+    { path: 'add', component: AddComponent },
+    { path: 'edit', component: EditComponent }
   ];
 
 
