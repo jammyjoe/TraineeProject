f0b94b3cd6ea7fa5bcd945a8175f27b20108e776
Jamil Munayem <jamilmunayem@hotmail.com>
2024-07-20 18:40:49 +0100

    destroy

--- Deployment/terraform_deploy.ps1
+++ Deployment/terraform_deploy.ps1
@@ -16,7 +16,7 @@ if(!$?) { echo "Unable to select workspace"; throw "Workspace Error"}
 terraform validate
 if(!$?) { echo "Invalid terraform"; throw "Validation Error"}
  
-terraform -destroy plan -out "terraform.deployment.tfplan" | tee terraform_plan_output.txt
+terraform plan -destroy -out "terraform.deployment.tfplan" | tee terraform_plan_output.txt
 if(!$?) { echo "Terraform plan failed"; throw "Plan Error"}
 
 terraform apply "terraform.deployment.tfplan"
