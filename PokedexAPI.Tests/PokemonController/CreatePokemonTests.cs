00f4986081f085deea19449291bfa89922cd9c46
Jamil Munayem <jamilmunayem@hotmail.com>
2024-08-12 16:26:09 +0100

    Unit Tests

---
+++ PokedexAPI.Tests/PokemonController/CreatePokemonTests.cs
@@ -0,0 +1,136 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Controllers;
+using Pokedex.DTOs;
+using Pokedex.RepositoryInterface;
+using PokedexAPI.Models;
+
+
+[TestFixture]
+public class CreatePokemonTests
+{
+    private PokedexContext _fakeContext;
+    private IPokemonRepository _fakePokemonRepository;
+    private IMapper _fakeMapper;
+    private PokemonController _fakePokemonController;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fakePokemonRepository = A.Fake<IPokemonRepository>();
+        _fakeMapper = A.Fake<IMapper>();
+        _fakePokemonController = new PokemonController(_fakeContext, _fakePokemonRepository, _fakeMapper);
+    }
+
+    [Test]
+    public async Task CreatePokemon_ReturnsBadRequest_WhenPokemonCreateIsNull()
+    {
+        // Arrange
+        PokemonDto pokemonCreate = null;
+
+        // Act
+        var result = await _fakePokemonController.CreatePokemon(pokemonCreate);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<BadRequestObjectResult>().Which
+            .Value.Should().Be("This Id is invalid");
+    }
+
+
+    [Test]
+    public async Task CreatePokemon_ReturnsCreatedAtActionResult_WhenSuccessful()
+    {
+        // Arrange
+        var pokemonCreate = A.Fake<PokemonDto>();
+        var createdPokemon = A.Fake<Pokemon>();
+        var createdPokemonDto = A.Fake<PokemonDto>();
+
+        A.CallTo(() => _fakePokemonRepository.PokemonExists(pokemonCreate.Name)).Returns(false);
+        A.CallTo(() => _fakePokemonRepository.ValidateDistinctTypes(pokemonCreate)).Returns(true);
+        A.CallTo(() => _fakePokemonRepository.AreStrengthsAndWeaknessesDistinct(pokemonCreate)).Returns(true);
+        A.CallTo(() => _fakePokemonRepository.CreatePokemon(pokemonCreate)).Returns(createdPokemon);
+        A.CallTo(() => _fakeMapper.Map<PokemonDto>(createdPokemon)).Returns(createdPokemonDto);
+
+        // Act
+        var result = await _fakePokemonController.CreatePokemon(pokemonCreate);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<CreatedAtActionResult>().Which
+            .Value.Should().Be(createdPokemonDto);
+        var createdAtActionResult = result.Result as CreatedAtActionResult;
+        createdAtActionResult.ActionName.Should().Be(nameof(_fakePokemonController.GetPokemon));
+        createdAtActionResult.RouteValues["name"].Should().Be(createdPokemonDto.Name);
+    }
+
+
+    [Test]
+    public async Task CreatePokemon_ReturnsBadRequest_WhenModelStateIsInvalid()
+    {
+        // Arrange
+        var pokemonCreate = A.Fake<PokemonDto>();
+        _fakePokemonController.ModelState.AddModelError("Name", "Required");
+
+        // Act
+        var result = await _fakePokemonController.CreatePokemon(pokemonCreate);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<BadRequestObjectResult>().Which
+            .Value.Should().Be(_fakePokemonController.ModelState);
+    }
+
+    [Test]
+    public async Task CreatePokemon_ReturnsBadRequest_WhenPokemonAlreadyExists()
+    {
+        // Arrange
+        var pokemonCreate = A.Fake<PokemonDto>();
+        A.CallTo(() => _fakePokemonRepository.PokemonExists(pokemonCreate.Name)).Returns(true);
+
+        // Act
+        var result = await _fakePokemonController.CreatePokemon(pokemonCreate);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<BadRequestObjectResult>().Which
+            .Value.Should().Be("This pokemon already exists");
+    }
+
+    [Test]
+    public async Task CreatePokemon_ReturnsBadRequest_WhenTypesAreNotDistinct()
+    {
+        // Arrange
+        var pokemonCreate = A.Fake<PokemonDto>();
+        A.CallTo(() => _fakePokemonRepository.ValidateDistinctTypes(pokemonCreate)).Returns(false);
+
+        // Act
+        var result = await _fakePokemonController.CreatePokemon(pokemonCreate);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<BadRequestObjectResult>().Which
+            .Value.Should().Be("Type 1 and Type 2 can not be the same");
+    }
+
+    [Test]
+    public async Task CreatePokemon_ReturnsBadRequest_WhenStrengthsAndWeaknessesAreNotDistinct()
+    {
+        // Arrange
+        var pokemonCreate = A.Fake<PokemonDto>();
+        A.CallTo(() => _fakePokemonRepository.ValidateDistinctTypes(pokemonCreate)).Returns(true);
+
+        A.CallTo(() => _fakePokemonRepository.AreStrengthsAndWeaknessesDistinct(pokemonCreate)).Returns(false);
+
+        // Act
+        var result = await _fakePokemonController.CreatePokemon(pokemonCreate);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<BadRequestObjectResult>().Which
+            .Value.Should().Be("Strengths and weaknesses cannot have duplicate types.");
+    }
+}
---
+++ PokedexAPI.Tests/PokemonController/GetPokemonTests.cs
@@ -0,0 +1,108 @@
+using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Controllers;
+using Pokedex.DTOs;
+using Pokedex.RepositoryInterface;
+using PokedexAPI.Models;
+
+[TestFixture]
+public class GetPokemonTests
+{
+    private PokedexContext _fakeContext;
+    private IPokemonRepository _fakePokemonRepository;
+    private IMapper _fakeMapper;
+    private PokemonController _fakePokemonController;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fakePokemonRepository = A.Fake<IPokemonRepository>();
+        _fakeMapper = A.Fake<IMapper>();
+        _fakePokemonController = new PokemonController(_fakeContext, _fakePokemonRepository, _fakeMapper);
+    }
+
+    [Test]
+    public async Task PokemonController_GetPokemons_ReturnsOkObjectResult()
+    {
+        // Arrange
+        var fakePokemonEntities = A.Fake<ICollection<Pokemon>>();
+        var fakePokemonDtos = A.Fake<List<PokemonDto>>();
+
+        A.CallTo(() => _fakePokemonRepository.GetPokemons()).Returns(fakePokemonEntities);
+        A.CallTo(() => _fakeMapper.Map<List<PokemonDto>>(fakePokemonEntities)).Returns(fakePokemonDtos);
+
+        // Act
+        var result = await _fakePokemonController.GetPokemons();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<OkObjectResult>()
+            .Which.Value.Should().BeSameAs(fakePokemonDtos);
+
+        A.CallTo(() => _fakePokemonRepository.GetPokemons()).MustHaveHappenedOnceExactly();
+        A.CallTo(() => _fakeMapper.Map<List<PokemonDto>>(fakePokemonEntities)).MustHaveHappenedOnceExactly();
+    }
+
+
+    [Test]
+    public async Task GetPokemon_ReturnsOkObjectResult_WhenPokemonExists()
+    {
+        // Arrange
+        var pokemonName = "ExistingPokemon";
+        var fakePokemonEntity = A.Fake<Pokemon>();
+        var fakePokemonDto = A.Fake<PokemonDto>();
+
+        A.CallTo(() => _fakePokemonRepository.PokemonExists(pokemonName)).Returns(true);
+        A.CallTo(() => _fakePokemonRepository.GetPokemon(pokemonName)).Returns(fakePokemonEntity);
+        A.CallTo(() => _fakeMapper.Map<PokemonDto>(fakePokemonEntity)).Returns(fakePokemonDto);
+
+        // Act
+        var result = await _fakePokemonController.GetPokemon(pokemonName);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<OkObjectResult>();
+        var okResult = result.Result as OkObjectResult;
+        okResult.Value.Should().Be(fakePokemonDto);
+    }
+
+    [Test]
+    public async Task GetPokemon_ReturnsNotFound_WhenPokemonDoesNotExist()
+    {
+        // Arrange
+        var pokemonName = "UnknownPokemon";
+
+        A.CallTo(() => _fakePokemonRepository.PokemonExists(pokemonName)).Returns(false);
+
+        // Act
+        var result = await _fakePokemonController.GetPokemon(pokemonName);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<NotFoundObjectResult>();
+        var notFoundResult = result.Result as NotFoundObjectResult;
+        notFoundResult.Value.Should().Be("This pokemon does not exist");
+    }
+
+    [Test]
+    public async Task GetPokemon_ReturnsNoContent_WhenModelStateIsInvalid()
+    {
+        // Arrange
+        var pokemonName = "ExistingPokemon";
+        _fakePokemonController.ModelState.AddModelError("Error", "Invalid ModelState");
+
+        A.CallTo(() => _fakePokemonRepository.PokemonExists(pokemonName)).Returns(true);
+        A.CallTo(() => _fakePokemonRepository.GetPokemon(pokemonName)).Returns(A.Fake<Pokemon>());
+
+        // Act
+        var result = await _fakePokemonController.GetPokemon(pokemonName);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<NoContentResult>();
+    }
+
+
+}
