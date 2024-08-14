ef96de3518ca5c2dd862ecd9b1f3f267164193ec
Jamil Munayem <jamilmunayem@hotmail.com>
2024-08-14 14:32:23 +0100

    Hardcode connection string and url

--- PokedexApp/src/services/pokemon.service.ts
+++ PokedexApp/src/services/pokemon.service.ts
@@ -7,8 +7,8 @@ import { Pokemon, PokemonType } from '../app/shared/models/pokemon.model';
   providedIn: 'root'
 })
 export class PokemonService {
-  private apiUrl = 'http://localhost:5019/api/Pokemon';
-  private typeApiUrl = 'http://localhost:5019/api/Type';
+  private apiUrl = 'https://your-api.azurewebsites.net/api/Pokemon'; 
+  private typeApiUrl = 'https://your-api.azurewebsites.net/api/Type'; 
 
   constructor(private http: HttpClient) {}
 
