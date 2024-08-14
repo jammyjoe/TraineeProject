6f7078d64d9386d5a3a15f2e626674d3a050f7e7
Jamil Munayem <jamilmunayem@hotmail.com>
2024-08-14 12:29:27 +0100

    change tf to use B1 sku

--- Deployment/main.tf
+++ Deployment/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_service_plan" "appserviceplan" {
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   os_type             = "Windows"
-  sku_name            = "F1"
+  sku_name            = "B1"
   worker_count        = "1" 
 
   tags = local.tags
