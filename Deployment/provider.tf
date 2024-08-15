012607d8c4d1107eb13f09e27fcd7f341280655e
Jamil Munayem <jamilmunayem@hotmail.com>
2024-08-15 12:16:04 +0100

    Create tfstate to lock infra

--- Deployment/provider.tf
+++ Deployment/provider.tf
@@ -1,22 +1,22 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "~> 3.97.0"
-#     }
-#   }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.97.0"
+    }
+  }
 
-#   backend "azurerm" {
-#     container_name = "tfstate"
-#     key            = "enpOrderShim.terraform.tfstate"
-#   }
-# }
+  backend "azurerm" {
+    container_name = "tfstate"
+    key            = "terraform.tfstate"
+  }
+}
 
 provider "azurerm" {
-  subscription_id = var.SUBSCRIPTION_ID
-  client_id       = var.CLIENT_ID
-  client_secret   = var.CLIENT_SECRET
-  tenant_id       = var.TENANT_ID
+    subscription_id = var.SUBSCRIPTION_ID
+    client_id       = var.CLIENT_ID
+    client_secret   = var.CLIENT_SECRET
+    tenant_id       = var.TENANT_ID
   features {
     key_vault {
       purge_soft_delete_on_destroy            = true
@@ -26,3 +26,4 @@ provider "azurerm" {
     }
   }
 }
+
