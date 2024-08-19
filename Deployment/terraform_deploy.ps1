b5ac7a03eeffb680193668e717423753ca8b5375
Jamil Munayem <jamilmunayem@hotmail.com>
2024-08-19 11:05:10 +0100

    coment imports

--- Deployment/terraform_deploy.ps1
+++ Deployment/terraform_deploy.ps1
@@ -16,10 +16,10 @@ if(!$?) { echo "Unable to select workspace"; throw "Workspace Error"}
 terraform validate
 if(!$?) { echo "Invalid terraform"; throw "Validation Error"}
 
-terraform import azurerm_resource_group.resource_group /subscriptions/975435ff-d720-4311-8ffc-536ad43592e4/resourceGroups/pokedex-dev-rg
-terraform import azurerm_mssql_server.pokedex_sqlserver /subscriptions/975435ff-d720-4311-8ffc-536ad43592e4/resourceGroups/pokedex-dev-rg/providers/Microsoft.Sql/servers/pokedex-dev-sqlserver
-terraform import azurerm_mssql_database.pokedex_db /subscriptions/975435ff-d720-4311-8ffc-536ad43592e4/resourceGroups/pokedex-dev-rg/providers/Microsoft.Sql/servers/pokedex-dev-sqlserver/databases/pokedex-dev-db
-terraform import azurerm_mssql_firewall_rule.allow_client_ip /subscriptions/975435ff-d720-4311-8ffc-536ad43592e4/resourceGroups/pokedex-dev-rg/providers/Microsoft.Sql/servers/pokedex-dev-sqlserver/firewallRules/allow-client-ip
+# terraform import azurerm_resource_group.resource_group /subscriptions/975435ff-d720-4311-8ffc-536ad43592e4/resourceGroups/pokedex-dev-rg
+# terraform import azurerm_mssql_server.pokedex_sqlserver /subscriptions/975435ff-d720-4311-8ffc-536ad43592e4/resourceGroups/pokedex-dev-rg/providers/Microsoft.Sql/servers/pokedex-dev-sqlserver
+# terraform import azurerm_mssql_database.pokedex_db /subscriptions/975435ff-d720-4311-8ffc-536ad43592e4/resourceGroups/pokedex-dev-rg/providers/Microsoft.Sql/servers/pokedex-dev-sqlserver/databases/pokedex-dev-db
+# terraform import azurerm_mssql_firewall_rule.allow_client_ip /subscriptions/975435ff-d720-4311-8ffc-536ad43592e4/resourceGroups/pokedex-dev-rg/providers/Microsoft.Sql/servers/pokedex-dev-sqlserver/firewallRules/allow-client-ip
 
 terraform plan -out "terraform.deployment.tfplan" | tee terraform_plan_output.txt
 if(!$?) { echo "Terraform plan failed"; throw "Plan Error"}
