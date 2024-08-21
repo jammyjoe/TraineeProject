2314fc56bc1250fa4854144df04c83987548276d
Jamil Munayem <jamilmunayem@hotmail.com>
2024-08-21 14:12:58 +0100

    change to use api url

--- Deployment/main.tf
+++ Deployment/main.tf
@@ -73,8 +73,8 @@ resource "azurerm_windows_web_app" "pokedex_webapi" {
     #"CORS_ALLOWED_ORIGINS"             = "https:/${azurem_windows_web_app.pokedex_webapp.name}.azurewebsites.net"
     #"AzureVault__Uri"                  = azurerm_key_vault.key_vault.vault_uri
     "SQL_CONNECTION_STRING"             = "Server=tcp:${azurerm_mssql_server.pokedex_sqlserver.name}.database.windows.net;Database=${azurerm_mssql_database.pokedex_db.name};User ID=jamil;Password=Password01!;Encrypt=true;Connection Timeout=30;"
-    "WEBSITE_ENABLE_SYNC_UPDATE_SITE"   = "true" 
-    "WEBSITE_RUN_FROM_PACKAGE"          = "0"
+    #"WEBSITE_ENABLE_SYNC_UPDATE_SITE"   = "true" 
+    #"WEBSITE_RUN_FROM_PACKAGE"          = "0"
   }
 }
 
