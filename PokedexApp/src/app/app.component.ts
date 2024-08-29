094f63d3d938af703db5ffa9755ad3138b54c1a2
Jamil Munayem <jamilmunayem@hotmail.com>
2024-08-29 12:25:12 +0100

    allow api calls to work via token

--- PokedexApp/src/app/app.component.ts
+++ PokedexApp/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, filter, switchMap } from 'rxjs';
 import { Pokemon } from './shared/models/pokemon.model';
 import { PokemonService } from '../services/pokemon.service';
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
 import { ExploreComponent } from './explore/explore.component';
+import { MsalBroadcastService, MsalRedirectComponent, MsalService } from '@azure/msal-angular';
+import { environment } from '../environments/environment';
+import { AuthenticationResult, EventType, InteractionStatus } from '@azure/msal-browser';
 
 @Component({
     selector: 'app-root',
@@ -15,17 +18,21 @@ import { ExploreComponent } from './explore/explore.component';
     styleUrls: ['./app.component.css'],
     imports: [AsyncPipe, CommonModule, RouterModule, NavigationComponent, ExploreComponent]
 })
+
 export class AppComponent implements OnInit {
+  constructor(private authService: MsalService) { }
+  
+  ngOnInit(): void {    
+      this.authService.handleRedirectObservable().subscribe();
+  }
+
+
   title = "PokemonApp";
-  pokemons$!: Observable<Pokemon[]>;
 
-  pokemonService = inject(PokemonService);
+  // pokemons$!: Observable<Pokemon[]>;
+  // pokemonService = inject(PokemonService);
 
-  ngOnInit(): void {
-    this.pokemons$ = this.pokemonService.getPokemons();
-  }
-
-  trackById(index: number, pokemon: Pokemon): number {
-    return pokemon.id;
-  }
+  // trackById(index: number, pokemon: Pokemon): number {
+  //   return pokemon.id;
+  // }
 }
--- PokedexApp/src/services/pokemon.service.ts
+++ PokedexApp/src/services/pokemon.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Observable, catchError, map, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Observable, catchError, map, switchMap, throwError } from 'rxjs';
 import { Pokemon, PokemonType } from '../app/shared/models/pokemon.model';
+import { environment } from '../environments/environment';
+import { MsalService } from '@azure/msal-angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,10 @@ export class PokemonService {
   // private apiUrl = 'https://pokedex-dev-web-api.azurewebsites.net/api/Pokemon'; 
   // private typeApiUrl = 'https://pokedex-dev-web-api.azurewebsites.net/api/Type'; 
 
-  private apiUrl = 'http://localhost:5019/api/Pokemon'; 
-  private typeApiUrl = 'http://localhost:5019/api/Type'; 
+  private apiUrl = `${environment.baseUrl}/api/Pokemon`;
+  private typeApiUrl = `${environment.baseUrl}api/Type`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private msalService: MsalService) {}
 
   private handleHttpError(error: HttpErrorResponse): Observable<never> {
     let errorMessage = '';
@@ -27,11 +29,31 @@ export class PokemonService {
     return throwError(() => new Error(errorMessage));
   }
 
+  private handleTokenError(error: any): Observable<never> {
+    // Handle errors related to token acquisition
+    console.error('Failed to acquire token:', error);
+    throw error;
+  }
+
   getPokemons(): Observable<Pokemon[]> {
-    return this.http.get<Pokemon[]>(this.apiUrl).pipe(
-      catchError(this.handleHttpError)
+    return this.msalService.acquireTokenSilent({ scopes: environment.scopeUri }).pipe(
+      switchMap(tokenResponse => {
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${tokenResponse.accessToken}` // Include the access token
+        });
+        return this.http.get<Pokemon[]>(this.apiUrl, { headers }).pipe(
+          catchError(this.handleHttpError) // Handle HTTP errors
+        );
+      }),
+      catchError(this.handleTokenError) // Handle token acquisition errors
     );
   }
+
+  // getPokemons(): Observable<Pokemon[]> {
+  //   return this.http.get<Pokemon[]>(this.apiUrl).pipe(
+  //     catchError(this.handleHttpError)
+  //   );
+  // }
 
   getPokemon(name: string): Observable<Pokemon> {
     const url = `${this.apiUrl}/${name}`;
