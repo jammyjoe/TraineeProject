9f1131873ba7e88e8ce3f7db5373837528b14158
Jamil Munayem <jamilmunayem@hotmail.com>
2024-09-03 13:39:32 +0100

    remove enabled_for_deployment as im not using vms

--- Deployment/key_vaults.tf
+++ Deployment/key_vaults.tf
@@ -7,7 +7,7 @@ resource "azurerm_key_vault" "key_vault" {
     sku_name                    = "standard"
     tenant_id                   = data.azurerm_client_config.current.tenant_id
     enable_rbac_authorization  = true
-    enabled_for_deployment      = true
+    #enabled_for_deployment      = true
     access_policy {
         tenant_id = data.azurerm_client_config.current.tenant_id
         object_id = data.azurerm_client_config.current.object_id
