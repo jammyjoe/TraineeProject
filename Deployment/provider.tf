662783d71a576e9fa6a27891b5706924d3c098b3
Jamil Munayem <jamilmunayem@hotmail.com>
2024-09-03 13:11:19 +0100

    uncomment service pricinpals

--- Deployment/provider.tf
+++ Deployment/provider.tf
@@ -16,9 +16,9 @@ terraform {
 
 provider "azurerm" {
   subscription_id = var.SUBSCRIPTION_ID
-  # client_id       = var.CLIENT_ID
-  # client_secret   = var.CLIENT_SECRET
-  # tenant_id       = var.TENANT_ID
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
+  tenant_id       = var.TENANT_ID
 
   features {
     key_vault {
--- Deployment/variables.tf
+++ Deployment/variables.tf
@@ -2,21 +2,21 @@ variable "SUBSCRIPTION_ID" {
   type = string
 }
 
-# variable "CLIENT_ID" {
-#   type = string
-# }
+variable "CLIENT_ID" {
+  type = string
+}
 
-# variable "CLIENT_SECRET" {
-#   type = string
-# }
+variable "CLIENT_SECRET" {
+  type = string
+}
 
-# variable "TENANT_ID" {
-#   type = string
-# }
+variable "TENANT_ID" {
+  type = string
+}
 
-# variable "ACCESS_KEY" {
-#   type = string
-# }
+variable "ACCESS_KEY" {
+  type = string
+}
 
 variable "ADMIN_PASSWORD" {
   type = string
