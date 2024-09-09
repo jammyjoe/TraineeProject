1b6f8f540e4d98719f6c0925a90ceb2d308bfe41
Jamil Munayem <jamilmunayem@hotmail.com>
2024-09-09 09:54:11 +0100

    temp import

--- Deployment/terraform_deploy.ps1
+++ Deployment/terraform_deploy.ps1
@@ -11,6 +11,8 @@ if(!$?) { echo "Unable to init Terraform"; throw "Init Error"}
 terraform validate
 if(!$?) { echo "Invalid terraform"; throw "Validation Error"}
 
+terraform import azurerm_mssql_database.pokedex_db /subscriptions/975435ff-d720-4311-8ffc-536ad43592e4/resourceGroups/pokedex-dev-rg/providers/Microsoft.Sql/servers/pokedex-dev-sqlserver/databases/pokedex-dev-db
+
 terraform plan -out "terraform.deployment.tfplan" | tee terraform_plan_output.txt
 if(!$?) { echo "Terraform plan failed"; throw "Plan Error"}
 
